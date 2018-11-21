ad5ed307260d40972e09e47e7b632fe02d03bd00
Atom Scott <atom.james.scott@gmail.com>
2018-11-21 12:37:42 +0900

    Add files via upload

--- seekbar.js
+++ seekbar.js
@@ -7,22 +7,22 @@ function Seekbar(cvs2, time){
   this.y = 0;
   this.previousX = 0;
   this.isDragged = false;
-      
+
   var length = cvs2.width;
   var height = cvs2.height;
-  
+
   if(!me.isDragged){
     me.x = time;
   }
-  
-   
+
+
   this.drawA = function(ctx2){
     ctx2.strokeStyle = "black";
     ctx2.strokeRect(0, 0, length, 100);
     ctx2.stroke();
-    
+
   };
-  
+
   this.drawRedLine = function(ctx2){
     ctx2.lineWidth = 2;
     ctx2.strokeStyle = "red";
@@ -31,23 +31,24 @@ function Seekbar(cvs2, time){
     ctx2.lineTo(me.x, height);
     ctx2.stroke();
   }
-  
+
+
   window.addEventListener("mousedown", function(e){
-    var dx = Math.abs(e.layerX - me.x);
-    console.log(me.x);
+    var dx = Math.abs(e.layerX - time);
     if(dx < 5){
-      console.log(dx);
+      clearInterval(animation)
       me.isDragged = true;
       me.x = e.layerX;
       isPaused = true;
     }
   });
-  
+
   window.addEventListener("mouseup", function(){
+    console.log("up")
     me.isDragged = false;
     isPaused = false;
   });
-  
+
   window.addEventListener("mousemove", function(e){
     if(me.isDragged){
       me.x = e.layerX;
